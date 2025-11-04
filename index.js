1a7873e39dcc9155e9ac3a4e339ecaf8472da7a9
alvaromoraromero <alvaro.moraromero@atexis.com>
2025-11-04 06:47:29 +0100

    [FEAT] Version inicial del juego

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let cambiarTonoModal, numPrInput, prisioneros;
+let cambiarTonoModal, numPrInput, prisioneros, restantes;
 const tonosPrisioneros = ['amarillo', 'claro', 'medio-claro', 'medio', 'medio-oscuro', 'oscuro'];
 
 window.onload= () => {
@@ -21,44 +21,87 @@ window.onload= () => {
 function cargarPrisioneros(num) {
     document.getElementById('totalPrisioneros').textContent = num;
     localStorage.setItem('numPrisioneros', num);
+    const restantesP = document.getElementById('restantes');
+    restantesP.style.marginLeft = '90px';
 
     prisioneros = [];
-    let divPrisioneros = document.querySelector('div#prisioneros');
-    if (!divPrisioneros) {
-        divPrisioneros = document.createElement('div');
-        divPrisioneros.id = 'prisioneros';
-        document.body.appendChild(divPrisioneros);
-    }
-    else divPrisioneros.innerHTML = '';
+    const divPrisioneros = document.getElementById('prisioneros');
+    divPrisioneros.innerHTML = '';
 
-    for (let i = 1; i <= num; i++) {
+    prisioneros.push({id: 0, sombrero: 'gris'});
+    for (let i = 1; i < num; i++) {
         const prisionero = {id: i, sombrero: Math.random() < 0.5 ? 'azul' : 'rojo', ocultarSombrero: true};
-        divPrisioneros.appendChild(crearPrisionero(prisionero));
         prisioneros.push(prisionero);
     }
 
-    let divMsgPrisioneros = document.querySelector('div#msgPrisioneros');
-    if (!divMsgPrisioneros) {
-        divMsgPrisioneros = document.createElement('div');
-        divMsgPrisioneros.id = 'msgPrisioneros';
-        document.body.appendChild(divMsgPrisioneros);
+    // Comprobar si todos tienen el mismo color
+    if (prisioneros.every(p => p.sombrero === prisioneros[1].sombrero)) {
+        // Forzamos que uno tenga el color contrario
+        const indiceAleatorio = Math.floor(Math.random() * num);
+        prisioneros[indiceAleatorio].sombrero = prisioneros[1].sombrero === 'azul' ? 'rojo' : 'azul';
     }
-    else divMsgPrisioneros.innerHTML = '';
+
+    const divMsgPrisioneros = document.getElementById('msgPrisioneros');
+    divMsgPrisioneros.innerHTML = '';
+
+    restantes = new Proxy({ rojo: 0, azul: 0}, {
+        set(target, prop, value) {
+            target[prop] = value;
+            const totalRestantes = Object.values(target).reduce((acc, val) => acc + val, 0);
+            restantesP.innerHTML = totalRestantes === 0 ? 'No tienes mas prisioneros por delante' : `Tienes <span style="color: red;">${target['rojo']}</span> prisioneros con sombrero <span style="color: red;">rojo</span> y <span style="color: blue;">${target['azul']}</span> prisioneros con sombrero <span style="color: blue;">azul</span> por delante`;
+        }
+    });
     for (const prisionero of prisioneros) {
-        const msgPrisionero = document.createElement('div');
-        msgPrisionero.id = `msgPrisionero${prisionero.id}`;
-        msgPrisionero.textContent = prisionero.id === 1 ? '***' : prisionero.sombrero;
-        console.log(prisionero.id);
-        if (prisionero.id > 2) msgPrisionero.style.opacity = '0';
-        divMsgPrisioneros.appendChild(msgPrisionero);
+        const divPrisionero = crearPrisionero(prisionero);
+        divPrisioneros.appendChild(divPrisionero);
+
+        if (prisionero.id === 0) {
+            const msgPrisionero = document.createElement('div');
+            msgPrisionero.id = `msgPrisionero${prisionero.id}`;
+            msgPrisionero.textContent = prisioneros.filter(p => p.sombrero === "rojo").length % 2 === 0 ? '🔴' : '🔵';
+            divMsgPrisioneros.appendChild(msgPrisionero);
+        }
+        else {
+            if (prisionero.id > 1) restantes[prisionero.sombrero]++;
+            const selectColor = document.createElement('div');
+            selectColor.id = `selectColor${prisionero.id}`;
+            const azul = document.createElement('div');
+            azul.title = 'azul';
+            azul.textContent = '🔵';
+            const rojo = document.createElement('div');
+            rojo.title = 'rojo';
+            rojo.textContent = '🔴';
+            selectColor.appendChild(azul);
+            selectColor.appendChild(rojo);
+            selectColor.childNodes.forEach((color) => {
+                color.addEventListener('click', () => {
+                    color.style.backgroundColor = color.title === prisionero.sombrero ? 'lime' : 'orange';
+                    selectColor.style.pointerEvents = 'none';
+                    divPrisionero.querySelector('img').classList.remove('gris');
+                    const nextSelectColor = document.getElementById(`selectColor${prisionero.id+1}`);
+                    if (nextSelectColor) {
+                        restantesP.style.marginLeft = `${64*(prisionero.id)+80*(prisionero.id+1)}px`;
+                        restantes[prisioneros[prisionero.id+1].sombrero]--;
+                        nextSelectColor.classList.remove('disabled');
+                    }
+                    else restantesP.innerHTML = '<span style="color: green">Partida finalizada</span>'
+                })
+            })
+            if (prisionero.id !== 1) selectColor.className = 'disabled';
+            divMsgPrisioneros.appendChild(selectColor);
+        }
     }
+}
+
+function finalizarPartida() {
+
 }
 
 function crearPrisionero(config = {sombrero: 'gris', ocultarSombrero: false}) {
     const prisionero = document.createElement('div');
     prisionero.className = 'prisionero';
 
-    if (config.id) prisionero.id = `prisionero${config.id}`;
+    if ('id' in config) prisionero.id = `prisionero${config.id}`;
 
     if (config.sombrero) {
         const sombrero = document.createElement('img');
